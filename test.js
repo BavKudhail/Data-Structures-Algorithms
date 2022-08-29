61c508ea63ab415e65a2901ec2dbc4ae335b70d0
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-08-29 16:26:43 +0100

    using hash-map data-structure

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-// the below is a way to solve the problem using a brute force method
 const a = [1, 2, 3, 9]; // []
 const b = [1, 2, 3, 4, 4]; // [4, 4]
 
-// linear search
-
-// business is a game of speed and problem solving
 const pairs = (array, sum) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
@@ -16,6 +12,39 @@ const pairs = (array, sum) => {
   return [];
 };
 
-console.log(pairs(b, 8));
+let array1 = ['a', 'b', 'c', 'd'];
+let array2 = ['e', 'f', 'z', 'h', 'd'];
 
-// binary search
+// brute force linear way of solving problem
+const common = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const commonPattern = (arr1, arr2) => {
+  let output = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!output[arr1[i]]) {
+      output[arr1[i]] = true;
+    }
+  }
+
+  console.log('output', output);
+
+  //  loop through the second array
+  for (let j = 0; j < arr2.length; j++) {
+    if (output[array2[j]]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(commonPattern(array1, array2));
